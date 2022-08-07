44e7f7738fbe87d23a42f03ebda77fca3bb501e9
Anirudh <anirudhrvs@gmail.com>
2022-08-07 10:33:28 -0700

    Implemnted logging

--- app.py
+++ app.py
@@ -3,12 +3,14 @@
 from flask import Flask, render_template, request,jsonify
 from flask_cors import CORS,cross_origin
 import pickle
-
+import logging
+logging.basicConfig(filename= 'app.log', level= logging.INFO, format= '%(levelname)s %(asctime)s %(name)s %(message)s', )
 app = Flask(__name__) # initializing a flask app
 
 @app.route('/',methods=['GET'])  # route to display the home page
 @cross_origin()
 def homePage():
+    logging.info('Rendering HTML template.')
     return render_template("index.html")
 
 @app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
@@ -28,15 +30,22 @@ def index():
                 research=1
             else:
                 research=0
+            logging.info('Model input taken from the user.')
+
+            # loading the model file from the storage
             filename = 'finalized_model.pickle'
-            loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
+            loaded_model = pickle.load(open(filename, 'rb'))
+            logging.info('Loaded the pickle file')
+
             # predictions using the loaded model file
             prediction=loaded_model.predict([[gre_score,toefl_score,university_rating,sop,lor,cgpa,research]])
+            logging.info('Prediction phase executed successfully.')
             print('prediction is', prediction)
             # showing the prediction results in a UI
+            logging.info('Final output displayed.')
             return render_template('results.html',prediction=round(100*prediction[0]))
         except Exception as e:
-            print('The Exception message is: ',e)
+            logging.exception('The Exception message is: ',e)
             return 'something is wrong'
     # return render_template('results.html')
     else:
